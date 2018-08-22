6ab809ab0618bbed4a3a199cb8276bd9cd181dc1
Albert Ventura <albert@Shankars-MBP.fios-router.home>
2018-08-22 13:45:21 -0700

    add search, news, json api category

--- www/script.js
+++ www/script.js
@@ -1,4 +1,61 @@
-  function getGar (){
+// search wordpress api json data 
+const getSearch = function(){
+  let searchVar = document.getElementById('input').value;
+  document.getElementById("root").innerHTML = "Search: " + searchVar;
+  getSe(searchVar);
+}
+document.getElementById("btn").addEventListener("click", getSearch);
+function getSe (n){
+  const se = 'http://venturads.com/weldinghelper/api/get_search_results/?search=' + n;
+
+document.getElementById("root").innerHTML = "<div class='loader'></div>";
+  // alert("loading...");
+fetch(se)
+.then(function(response){
+  return response.json();
+  })
+.then(function(data){
+   getdata(data);
+})
+
+function getdata(data){
+  let list="";
+  for(let i=0;i<data.posts.length;i++){
+    list += "<div class='btn-group-justified content'><a  type='button' class='btn btn-info sharp' id='settings' data-toggle='collapse' data-target='#demo" + data.posts[i].id + "'>" +  data.posts[i].title + "</a></div><div id='demo" +  data.posts[i].id + "' class='collapse'>" + data.posts[i].content + "</div></div>";
+    }
+  document.getElementById("root").innerHTML = list;
+}
+  
+}
+document.getElementById('btn').addEventListener('click', getSe);
+
+// get wordpress api json data
+function getNews (){
+  const news1 = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=82413d9fd4ad42528ed2caeb0473d089';
+
+document.getElementById("root").innerHTML = "<div class='loader'></div>";
+  // alert("loading...");
+fetch(news1)
+.then(function(response){
+  return response.json();
+  })
+.then(function(data){
+   getdata(data);
+})
+
+function getdata(data){
+  let list="";
+  for(let i=0;i<data.articles.length;i++){
+    list += "<div id='pic'><a href='" + data.articles[i].url + "' target='_blank'><img src='" + data.articles[i].urlToImage + "' width='100%' /><p class='center'>" + data.articles[i].title + "</p></a></div>";
+    }
+  document.getElementById("root").innerHTML = list;
+}
+  
+}
+document.getElementById('content-news').addEventListener('click', getNews);
+
+// get wordpress api json data
+function getGar (){
     const n2 = 'http://venturads.com/weldinghelper/api/get_category_posts/?slug=welding';
 
   document.getElementById("root").innerHTML = "<div class='loader'></div>";
@@ -22,6 +79,7 @@
   }
   document.getElementById('content-app').addEventListener('click', getGar);
   
+// more menu list
   function getMenu (){
     // const n2 = 'https://venturads.com/helper-menu.json';
 
@@ -59,6 +117,7 @@
   }
   document.getElementById('content-menu').addEventListener('click', getMenu);
 
+  //admob id script
   function onDeviceReady() {
     document.removeEventListener('deviceready', onDeviceReady, false);
 
