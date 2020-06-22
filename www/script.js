89618593df7667fd91850f9b43e4faa515b4629d
venturads <albert@venturads.com>
2020-06-21 22:15:39 -0700

    revert to prev ux

--- www/script.js
+++ www/script.js
@@ -1,189 +1,275 @@
-//close navbar menu on link click
-document.querySelector('.mdl-layout__drawer').addEventListener('click', function () {
-  document.querySelector('.mdl-layout__obfuscator').classList.remove('is-visible');
-  this.classList.remove('is-visible');
-}, false);
-
-//go home page
-function getHome() {
-  document.getElementById('root').innerHTML = "<img src='./img/welding.gif' width='100%' height='100%'></div></div>";
+// search wordpress api json data 
+const getSearch = function(){
+  let searchVar = document.getElementById('input').value;
+  document.getElementById("root").innerHTML = "Search: " + searchVar;
+  getSe(searchVar);
 }
-document.getElementById('home-btn').addEventListener('click', getHome);
-document.getElementById('demo-avatar').addEventListener('click', getHome);
+// document.getElementById("btn").addEventListener("click", getSearch);
+function getSe (n){
+  const se = 'http://venturads.com/weldinghelper/api/get_search_results/?search=' + n;
+
+document.getElementById("root").innerHTML = "<div class='loader'></div>";
+  // alert("loading...");
+fetch(se)
+.then(function(response){
+  return response.json();
+  })
+.then(function(data){
+   getdata(data);
+})
+
+function getdata(data){
+  let list="";
+  for(let i=0;i<data.posts.length;i++){
+    list += "<div class='btn-group-justified content'><a  type='button' class='btn btn-info sharp' id='settings' data-toggle='collapse' data-target='#demo" + data.posts[i].id + "'>" +  data.posts[i].title + "</a></div><div id='demo" +  data.posts[i].id + "' class='collapse'>" + data.posts[i].content + "</div></div>";
+    }
+  document.getElementById("root").innerHTML = list;
+}
+  
+}
+// document.getElementById('btn').addEventListener('click', getSe);
+
+// get wordpress api json data
+function getNews (){
+  const news1 = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=82413d9fd4ad42528ed2caeb0473d089';
+
+document.getElementById("root").innerHTML = "<div class='loader'></div>";
+  // alert("loading...");
+fetch(news1)
+.then(function(response){
+  return response.json();
+  })
+.then(function(data){
+   getdata(data);
+})
+
+function getdata(data){
+  let list="";
+  for(let i=0;i<data.articles.length;i++){
+    list += "<div id='pic'><a href='" + data.articles[i].url + "' target='_blank'><img src='" + data.articles[i].urlToImage + "' width='100%' /><p class='center'>" + data.articles[i].title + "</p></a></div>";
+    }
+  document.getElementById("root").innerHTML = list;
+}
+  
+}
+// document.getElementById('content-news').addEventListener('click', getNews);
+
+// get wordpress api json data
+function getGar (){
+    //const n2 = 'http://venturads.com/weldinghelper/api/get_category_posts/?slug=welding';
+    const n2 = 'https://venturads.com/json/weld.json';
+
+  document.getElementById("root").innerHTML = "<div class='loader'></div>";
+    // alert("loading...");
+  fetch(n2)
+  .then(function(response){
+    return response.json();
+    })
+  .then(function(data){
+     getdata(data);
+  })
+  
+  function getdata(data){
+    let list="";
+    for(let i=0;i<data.data.length;i++){
+      list += "<div class='btn-group-justified content'><a  type='button' class='btn btn-info sharp' id='settings' data-toggle='collapse' data-target='#demo" + data.data[i].id + "'>" +  data.data[i].title + "</a></div><div id='demo" +  data.data[i].id + "' class='collapse'>" + data.data[i].content + "</div></div>";
+      }
+    document.getElementById("root").innerHTML = list;
+  }
+    
+  }
+
 // more menu list
 function getData (){
   // const n2 = 'https://venturads.com/helper-menu.json';
-  getHome();
-document.getElementById("content-app").innerHTML = "<div class='loader'></div>";
+
+document.getElementById("root").innerHTML = "<div class='loader'></div>";
   let data=[
     {
       id: 2020,
-      title: "Chart",
+      title: "Setting Up",
       content: "Welding Chart",
       url: "img/1.jpg",
       urlToImage: "img/1.jpg"
     },{
       id: 2021,
-      title: "Symbols",
+      title: "Connect Torch",
       content: "Symbols",
       url: "img/2.jpg",
       urlToImage: "img/2.jpg"
     },{
       id: 2022,
-      title: "Specs",
+      title: "Foot Pedal",
       content: "Specs",
       url: "img/3.jpg",
       urlToImage: "img/3.jpg"
     },{
       id: 2023,
-      title: "Settings",
+      title: "Polarity",
       content: "Settings",
       url: "img/4.jpg",
       urlToImage: "img/4.jpg"
     },{
       id: 2024,
-      title: "Basic Symbols",
+      title: "Grind Tungsten",
       content: "Basic Symbols",
       url: "img/5.jpg",
       urlToImage: "img/5.jpg"
     },  {
       id: 2025,
-      title: "Chart",
+      title: "Gas Flow",
       content: "Welding Chart",
       url: "img/6.jpg",
       urlToImage: "img/6.jpg"
     },{
       id: 2026,
-      title: "Data",
+      title: "Amperage",
       content: "Data",
       url: "img/7.jpg",
       urlToImage: "img/7.jpg"
     },{
       id: 2027,
-      title: "Types",
+      title: "Clean Materials",
       content: "Types",
       url: "img/8.jpg",
       urlToImage: "img/8.jpg"
     },{
       id: 2028,
-      title: "More Types",
+      title: "Electrode",
       content: "More Types",
       url: "img/9.jpg",
       urlToImage: "img/9.jpg"
     },{
       id: 2029,
-      title: "Weld Joints",
+      title: "Clamps",
       content: "Weld Joints",
       url: "img/10.jpg",
       urlToImage: "img/10.jpg"
     },{
       id: 2030,
-      title: "Process",
+      title: "Tack Weld",
       content: "Process",
       url: "img/11.jpg",
       urlToImage: "img/11.jpg"
     },{
       id: 2031,
-      title: "Safety",
+      title: "TIG Torch",
       content: "Safety",
       url: "img/12.jpg",
       urlToImage: "img/12.jpg"
     },{
       id: 2032,
-      title: "Equipment",
+      title: "Pedal Control",
       content: "Equipment",
       url: "img/13.jpg",
       urlToImage: "img/13.jpg"
     },{
       id: 2033,
-      title: "Workbench",
+      title: "Filler",
       content: "Workbench",
       url: "img/14.jpg",
       urlToImage: "img/14.jpg"
     },{
       id: 2034,
-      title: "Seated Workbench",
+      title: "Base Metal",
       content: "Seated Workbench",
       url: "img/15.jpg",
       urlToImage: "img/15.jpg"
     },  {
       id: 2035,
-      title: "Mill Direction",
+      title: "Puddle",
       content: "Mill Direction",
       url: "img/16.jpg",
       urlToImage: "img/16.jpg"
     },{
       id: 2036,
-      title: "Cutting Speeds",
+      title: "Fillet",
       content: "Cutting Speeds",
       url: "img/17.jpg",
       urlToImage: "img/17.jpg"
     },{
       id: 2037,
-      title: "Decimal Chart",
+      title: "Lap Joint",
       content: "Decimal Chart",
       url: "img/18.jpg",
       urlToImage: "img/18.jpg"
     },{
       id: 2038,
-      title: "Rpm",
+      title: "T-joint",
       content: "Rpm",
       url: "img/19.jpg",
       urlToImage: "img/19.jpg"
     },{
       id: 2039,
-      title: "Tap/Drill Chart",
+      title: "Corner Joint",
       content: "Tap/Drill Chart",
       url: "img/20.jpg",
       urlToImage: "img/20.jpg"
     },{
       id: 2040,
-      title: "Inch/Metric Chart",
+      title: "Butt Weld",
       content: "Inch/Metric Chart",
       url: "img/21.jpg",
       urlToImage: "img/21.jpg"
-    },{
-      id: 2041,
-      title: "Sheet Metal Charts",
-      content: "Sheet Metal Charts",
-      url: "img/22.jpg",
-      urlToImage: "img/22.jpg"
-    },{
-      id: 2042,
-      title: "Weld Details",
-      content: "Weld Details",
-      url: "img/23.jpg",
-      urlToImage: "img/23.jpg"
-    },{
-      id: 2043,
-      title: "Metal Inert Gas",
-      content: "Metal Inert Gas",
-      url: "img/24.jpg",
-      urlToImage: "img/24.jpg"
     }
   ];
   let list = "";
+  console.log("mylist: " + data[0].title);
   for(let i=0;i<data.length;i++){
-    list += "<a href=#" + data[i].id + " class='mdl-navigation__link' type='button' id=" + data[i].id + " class='btn btn-primary sharp'>" +  data[i].title + "</a></br>";
-    document.getElementById('content-app').innerHTML = list;
-  }
-  for(let i=0;i<data.length;i++){ 
-    function getImage() {
-      document.getElementById('root').innerHTML = "<img src='" + data[i].url + "' width='100%' ></div></div>";
-    }
-    document.getElementById(data[i].id).addEventListener('click', getImage);
-  }
+    list += "<div class='btn-group-justified content'><a  type='button' class='btn btn-info sharp' id='settings' data-toggle='collapse' data-target='#demo" + data[i].id + "'>" +  data[i].title + "</a></div><div id='demo" +  data[i].id + "' class='collapse'><img src='" + data[i].url + "' width='100%' ></div></div>";
+  document.getElementById('root').innerHTML = list;
+}
+  
 }
 
-getData();
+document.getElementById('content-app').addEventListener('click', getData);
+  
+// more menu list
+  function getMenu (){
+    // const n2 = 'https://venturads.com/helper-menu.json';
+
+  document.getElementById("root").innerHTML = "<div class='loader'></div>";
+    let data=[
+      {
+        id: 1, name: "RPM Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.RPM_HELPER"
+      },
+      {
+        id: 2, name: "Endline puzzle game", link: "https://play.google.com/store/apps/details?id=com.venturads.games.EndLine"
+      },
+      {
+        id: 3, name: "CNC Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.CNCHelperPlay"
+      },
+      {
+        id: 4, name: "Solidworks Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.albert.SolidworksHelper"
+      },
+      {
+        id: 5, name: "Ballon Pop Free puzzle game", link: "https://play.google.com/store/apps/details?id=com.venturads.BalloonPopFree"
+      },
+      {
+        id: 6, name: "Sheet Metal Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.SheetMetalHelper"
+      },
+      {
+        id: 6, name: "Venturads.com", link: "https://venturads.com"
+      }
+    ];
+    let list = "";
+    console.log("mylist: " + data[0].name);
+    for(let i=0;i<data.length;i++){
+      list += "<div class='btn-group-justified content'><a href='" +  data[i].link + "' type='button' class='btn btn-info sharp' id='settings' data-target='#demo" + data[i].id + "' target='_blank'>" +  data[i].name + "</a></div><div id='demo" +  data[i].id + "' class='collapse'>" + data[i].name + "</div></div>";
+    document.getElementById('root').innerHTML = list;
+  }
+    
+  }
+  document.getElementById('content-menu').addEventListener('click', getMenu);
 
   //admob id script
   function onDeviceReady() {
     document.removeEventListener('deviceready', onDeviceReady, false);
+
     // Set AdMobAds options:
     admob.setOptions({
-        publisherId:          "ca-app-pub-3864996818231266/6846758432",  // Required
-        interstitialAdId:     "ca-app-pub-3864996818231266/6580570832",  // Optional
+      publisherId:          "ca-app-pub-3864996818231266/6846758432",  // Required
+      interstitialAdId:     "ca-app-pub-3864996818231266/6580570832",  // Optional
         // tappxIdiOS:           "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
         // tappxIdAndroid:       "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
         // tappxShare:           0.5                                        // Optional
